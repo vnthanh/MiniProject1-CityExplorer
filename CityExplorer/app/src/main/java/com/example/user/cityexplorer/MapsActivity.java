8d2f38a9615bd65d1d347960817e3b10648379b2
vnthanh <vnthanh3004@gmail.com>
2016-04-23 22:49:30 +0700

    Find direction, bugs: find after more click

--- CityExplorer/app/src/main/java/com/example/user/cityexplorer/MapsActivity.java
+++ CityExplorer/app/src/main/java/com/example/user/cityexplorer/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.example.user.cityexplorer;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.AutocompleteFilter;
@@ -15,17 +21,45 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.datatype.Duration;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     ArrayList<LatLng> Places = new ArrayList<>();
 
+    final String REQUEST_LINK = "https://maps.googleapis.com/maps/api/directions/";
+    final String API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    String strRequestLink;
+
+    EditText et_origin,et_dest;
+
+    private List<Marker> originMarkers = new ArrayList<>();
+    private List<Marker> destinationMarkers = new ArrayList<>();
+    private List<Polyline> polylinePaths = new ArrayList<>();
+    public List<Route> routes = new ArrayList<Route>();
+
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +76,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Places.add(new LatLng(10.780100, 106.699016));
         Places.add(new LatLng(10.767140, 106.640752));
 
+        et_origin = (EditText) findViewById(R.id.et_originPos);
+        et_dest = (EditText) findViewById(R.id.et_destPos);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -63,9 +90,200 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions().position(Places.get(i)).title("Place:"+ i));
         }
 
-        mMap.animateCamera(CameraUpdateFactory.zoomBy(15));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(20));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Places.get(0)));
 
     }
 
+    public void bt_findDirection_Clicked(View view)
+    {
+        String startPos = et_origin.getText().toString();
+        String destPos = et_dest.getText().toString();
+
+        strRequestLink = REQUEST_LINK + "json" +
+                "?origin=" + startPos.replace(" ", "+") +
+                "&destination=" + destPos.replace(" ", "+") +
+                "&key=" + API_KEY;
+
+
+        BackGroundTask backGroundTask = new BackGroundTask();
+        backGroundTask.execute();
+
+        for (Route route : routes) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+                /*((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
+                ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);*/
+
+            originMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+            destinationMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions().
+                    geodesic(true).
+                    color(Color.BLUE).
+                    width(10);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add(mMap.addPolyline(polylineOptions));
+        }
+    }
+
+
+    private class BackGroundTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //Toast.makeText(MainActivity.this, "Begin", Toast.LENGTH_SHORT).show();
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage("Working on it...");
+            //pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try{
+                URL url = new URL(strRequestLink);
+                InputStream inputStream = url.openConnection().getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                try {
+                    routes = parseJSon(buffer.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                return buffer.toString();
+            }
+            catch (MalformedURLException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+        }
+    }
+
+    public class Distance {
+        public String text;
+        public int value;
+
+        public Distance(String text, int value) {
+            this.text = text;
+            this.value = value;
+        }
+    }
+
+    public class Duration {
+        public String text;
+        public int value;
+
+        public Duration(String text, int value) {
+            this.text = text;
+            this.value = value;
+        }
+    }
+
+    public class Route {
+        public Distance distance;
+        public Duration duration;
+        public String endAddress;
+        public LatLng endLocation;
+        public String startAddress;
+        public LatLng startLocation;
+
+        public List<LatLng> points;
+    }
+
+    private List<Route> parseJSon(String data) throws JSONException {
+        if (data == null)
+            return null;
+
+        List<Route> routes = new ArrayList<Route>();
+        JSONObject jsonData = new JSONObject(data);
+        JSONArray jsonRoutes = jsonData.getJSONArray("routes");
+        for (int i = 0; i < jsonRoutes.length(); i++) {
+            JSONObject jsonRoute = jsonRoutes.getJSONObject(i);
+            Route route = new Route();
+
+            JSONObject overview_polylineJson = jsonRoute.getJSONObject("overview_polyline");
+            JSONArray jsonLegs = jsonRoute.getJSONArray("legs");
+            JSONObject jsonLeg = jsonLegs.getJSONObject(0);
+            JSONObject jsonDistance = jsonLeg.getJSONObject("distance");
+            JSONObject jsonDuration = jsonLeg.getJSONObject("duration");
+            JSONObject jsonEndLocation = jsonLeg.getJSONObject("end_location");
+            JSONObject jsonStartLocation = jsonLeg.getJSONObject("start_location");
+
+            route.distance = new Distance(jsonDistance.getString("text"), jsonDistance.getInt("value"));
+            route.duration = new Duration(jsonDuration.getString("text"), jsonDuration.getInt("value"));
+            route.endAddress = jsonLeg.getString("end_address");
+            route.startAddress = jsonLeg.getString("start_address");
+            route.startLocation = new LatLng(jsonStartLocation.getDouble("lat"), jsonStartLocation.getDouble("lng"));
+            route.endLocation = new LatLng(jsonEndLocation.getDouble("lat"), jsonEndLocation.getDouble("lng"));
+            route.points = decodePolyLine(overview_polylineJson.getString("points"));
+
+            routes.add(route);
+        }
+        return routes;
+        //listener.onDirectionFinderSuccess(routes);
+    }
+
+    private List<LatLng> decodePolyLine(final String poly) {
+        int len = poly.length();
+        int index = 0;
+        List<LatLng> decoded = new ArrayList<LatLng>();
+        int lat = 0;
+        int lng = 0;
+
+        while (index < len) {
+            int b;
+            int shift = 0;
+            int result = 0;
+            do {
+                b = poly.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = poly.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            decoded.add(new LatLng(
+                    lat / 100000d, lng / 100000d
+            ));
+        }
+
+        return decoded;
+    }
 }
